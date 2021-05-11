78d3a206c9157d833f5ebf42bd96d31b78055759
brduarte <brunomacielduarte32@gmail.com>
2021-05-10 23:44:49 -0300

    salvando trabalho

--- src/database/migrations/1620700697821-Create-Settings-Jobs.ts
+++ src/database/migrations/1620700697821-Create-Settings-Jobs.ts
@@ -1,11 +1,22 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateSettingsJobs1620700697821 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "settings_jobs",
+        columns: [
+          {
+            name: "id",
+            type: "binary",
+            isPrimary: true,
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+      queryRunner.dropTable('settings_jobs')
+  }
 }
