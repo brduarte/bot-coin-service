629ba7cb3566dea3ee055536e7193c4b57f4036e
brduarte <brunomacielduarte32@gmail.com>
2021-05-18 00:35:06 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,16 @@ import { Router } from "express";
 import { SchedulesJobsController } from "./controllers/SchedulesJobsController";
 import { CandlesController } from "./controllers/CandlesController";
 
-
-
 const routes = Router();
 
+routes.get("/", (request, response) => {
+    return response.json({
+        version: '1.0',
+        description: 'Bot Coin Service' 
+    })
+});
+
 routes.post("/jobs", new SchedulesJobsController().create);
-routes.get("/candles", new CandlesController().getCandles)
+routes.get("/candles", new CandlesController().getCandles);
 
 export { routes };
